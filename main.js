4ca5251258e6b89fd339f5f222505b3d3d2f7236
Robin Sevilla <ryssevilla25@gmail.com>
2020-08-17 17:49:04 +0800

    L5-start

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 1234, color: 'red' },
+                { id: 1235, color: 'blue' },
+            ],
+            sizes : [
+                { id: 'sds', sizess: 'small' },
+                { id: 13, sizess: 'medium' },
+                { id: 14, sizess: 'large' },
+            ]
         }
     }
 })
